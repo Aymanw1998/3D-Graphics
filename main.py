e526b7c2ad358a26f86039cd2ebaeaf47c8607b8
sapire <sapir8886@gmail.com>
2021-06-12 21:56:03 +0300

    update

---
+++ main.py
@@ -0,0 +1,23 @@
+# TODO:
+# projections - הטלות דו מימדיות של האובייקים התלת מימדיים
+# scaling - סילום, הגדלה והקטנה של צמד האובייקטים
+# rotation - סיבוב צמד אובייקטים
+# hideen surface removal- הסרת משטחים נסתרים
+
+def parallel_projection():  # הטלה מקבילית
+    pass
+
+def perspective_projection():  # הטלה פרספקטיבית
+    pass
+
+def oblique_projections():  # הטלה אלכסונית
+    pass
+
+def scale():  # סילום
+    pass
+
+def rotate():  # סיבוב
+    pass
+
+def remove_hidden_surface():  # הסרת משטחים נסתרים
+    pass
