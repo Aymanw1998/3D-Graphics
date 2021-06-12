88fe2ba069f0526239f7a0e46277fe4f22497b84
sapire <sapir8886@gmail.com>
2021-06-12 22:34:17 +0300

    update

--- main.py
+++ main.py
@@ -32,12 +32,36 @@ def remove_hidden_surface():  # הסרת משטחים נסתרים
     pass
 
 
-class Point3D:
+class Vertex:
+    """Each vertex is a 3D point that consists of three values: x, y, z."""
     def __init__(self, x, y, z) -> None:
         self.x = x
         self.y = y
         self.z = z
 
+class Edges:
+    """An edge represents the line between two vertices."""
+    def __init__(self, v1, v2) -> None:
+        self.v1 = v1
+        self.v2 = v2
+        pass
 
-vertices_list = [] # is a list of 3d points
-polygon_list = [] # is a list of vertices
+class Polygon:
+    def __init__(self, e1, e2, e3, e4=None) -> None:
+        self.e1 = e1
+        self.e2 = e2
+        self.e3 = e3
+        self.e4 = e4
+        pass
+
+edges_list = []
+polygon_list = []
+
+def calculate_visability():  # help function for surface removal
+    pass
+
+def calculate_normal_vector():  # help function for surface removal
+    pass
+
+def sort_by_z():  # so we know which object is the farthest away so we can draw it first
+    pass
