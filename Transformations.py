e8de4a042242db7e1360e84c4be138fe5d6560c0
sapire <sapir8886@gmail.com>
2021-06-13 08:57:31 +0300

    Split code into files

---
+++ Transformations.py
@@ -0,0 +1,49 @@
+################# Transformations######################
+
+def parallel_projection():  # הטלה מקבילית
+    # note to self: we need to ask the user for an angle!!
+    pass
+
+def perspective_projection():  # הטלה פרספקטיבית
+    # we need to ask the user for an angle!!
+    pass
+
+def oblique_projections():  # הטלה אלכסונית
+    # note to self: we need to ask the user for an angle!!
+    pass
+
+def scale_up():  # סילום
+    pass
+
+def scale_down():
+    pass
+
+def rotate_x_axis():  # סיבוב
+    # note to self: we need to ask the user for an angle!!
+    pass
+
+def rotate_y_axis():
+    pass
+
+def rotate_z_axis():
+    pass
+
+def remove_hidden_surface():  # הסרת משטחים נסתרים
+    pass
+
+
+################## Help functions###############
+
+def calculate_visability():  # help function for surface removal
+    """We need to know the visibility of an object to help us remove hidden surfaces."""
+    pass
+
+def calculate_normal_vector():  # help function for surface removal
+    """To perform the visibility calculation in 'calculate_visibility' we must calculate the normal vector."""
+    pass
+
+def sort_by_z():  # so we know which object is the farthest away so we can draw it first
+    """The purpose of this method is to arrage the objects by their z-axis value, so we know which object is the farthest away
+    and which is closest to our eyes.
+    The objects will be drawn to the screen by this order: first we draw the farthest object and work our way to the closest one."""
+    pass
